5736823231fbef1cc69499ec9458f9e5aebe1c6c
bibekbhandari <bhandaribibek27@gamil.com>
2025-01-13 17:18:08 +0545

    get, post, patch,delete

--- todo_api/views.py
+++ todo_api/views.py
@@ -5,28 +5,81 @@ from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
 
-class TodoListApiView(APIView):
-    permission_classes= [permissions.IsAuthenticated]
 
-    def get(self,request, *args, **kwargs):
+class TodoListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
         """
         List all the todo items for the given requested user
         """
-        todos = Todo.objects.filter(user= request.user.id)
-        serializer = TodoSerializer(todos, many= True)
+        todos = Todo.objects.filter(user=request.user.id)
+        serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self,request,*args, **kwargs):
-        """create the Todo with the given data
-        """
-        data={
-            'task': request.data.get('task'),
-            'completed': request.data.get('completed'),
-            'user': request.user.id
+
+    def post(self, request, *args, **kwargs):
+        """create the Todo with the given data"""
+        data = {
+            "task": request.data.get("task"),
+            "completed": request.data.get("completed"),
+            "user": request.user.id,
         }
-        serialzer= TodoSerializer(data=data)
+        serialzer = TodoSerializer(data=data)
         if serialzer.is_valid():
             serialzer.save()
-            return Response(serialzer.data,status=status.HTTP_201_CREATED)
-        return Response(serialzer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serialzer.data, status=status.HTTP_201_CREATED)
+        return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetailApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, todo_id, user_id):
+        """helper mehtod to get the object with todo_id and user_id"""
+        try:
+            return Todo.objects.get(id=todo_id, user=user_id)
+        except Todo.DoesNotExist:
+            return None
+
+    def get(self, request, todo_id, *args, **kwargs):
+        """Retrive the todo with given todo_id"""
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"error": "object with id donot exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        Serializer = TodoSerializer(todo_instance)
+        return Response(Serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, todo_id, *args, **kwargs):
+        """Updates the todo item with given todo_id if exists"""
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"error": "objects with the given data doesnot exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        data = {
+            "task": request.data.get("task"),
+            "completed": request.data.get("completed", todo_instance.completed),
+            "user": request.user.id,
+        }
+        serializer = TodoSerializer(instance=todo_instance,data= data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
+    def delete(self,request,todo_id, *args, **kwargs):
+        '''Deletes the todo item with the given id'''
+        todo_instance = self.get_object(todo_id,request.user.id)
+        if not todo_instance:
+            return Response(
+                {"error":"object with given id not matched"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        todo_instance.delete()
+        return Response(
+            {"ok":"data udyo"}, status=status.HTTP_410_GONE
+        )
